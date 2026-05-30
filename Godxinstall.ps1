305c01897fc1a7f692f991af49dd9d3ceb001abb
arxstore <paphawin.ffaith@gmail.com>
2026-05-31 01:02:07 +0700

    Update Godxinstall.ps1

--- Godxinstall.ps1
+++ Godxinstall.ps1
@@ -1,7 +1,9 @@
 $tempBat = Join-Path $env:TEMP "Setting GodX.bat"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arxstore/settinggodx/refs/heads/main/Setting%20GodX.bat" -OutFile $tempBat
+$batUrl = "https://raw.githubusercontent.com/arxstore/settinggodx/refs/heads/main/Setting%20GodX.bat"
 
-Start-Process $tempBat -Wait
+Invoke-WebRequest -Uri $batUrl -OutFile $tempBat
 
-Remove-Item $tempBat -Force -ErrorAction SilentlyContinue
+Start-Process -FilePath $tempBat -Wait
+
+Remove-Item -Path $tempBat -Force -ErrorAction SilentlyContinue
