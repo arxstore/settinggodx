545a2ae6786a114b9011a9fe311e278c86952054
arxstore <paphawin.ffaith@gmail.com>
2026-05-31 00:51:09 +0700

    Delete Godxinstall.ps1

--- Godxinstall.ps1
+++
@@ -1,9 +0,0 @@
-$BatFilePath = "$env:TEMP\Setting GodX.bat"
-
-$GithubUrl = "https://raw.githubusercontent.com/arxstore/settinggodx/refs/heads/main/Setting%20GodX.bat"
-
-Invoke-WebRequest -Uri $GithubUrl -OutFile $BatFilePath
-
-& "$BatFilePath"
-
-Remove-Item -Path $BatFilePath -Force
